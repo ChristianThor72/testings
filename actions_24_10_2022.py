855e0c51ead17aeeacf3378111650f18e9fb33d1
Christian Moller <pst528@alumni.ku.dk>
2022-10-26 14:50:46 +0200

    flemming din tosse 10

--- actions_24_10_2022.py
+++ actions_24_10_2022.py
@@ -76,7 +76,7 @@ def drive_to_object(dist_mm, ang, sign):
 def scan_for_object(cam,dict):
     for _ in range(18):
         turn_degrees(20, 1) #Turning right
-        sleep(0.75) #Sleep time it takes to turn 30 degrees.
+        sleep(1.5) #Sleep time it takes to turn 30 degrees.
         temp_frame = cam.get_next_frame()
         corners, ids, rejected = cv2.aruco.detectMarkers(temp_frame, dict)
         if corners:
