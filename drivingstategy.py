39c8fd770b0a9a5796c5b59c0c3f241502ee86c7
Christian Moller <pst528@alumni.ku.dk>
2022-10-26 14:37:27 +0200

    flemming din tosse 10

--- drivingstategy.py
+++ drivingstategy.py
@@ -82,7 +82,7 @@ while not finished:
          
          #Check if it is close to id 1
          sleep(5)
-         actions.backward_m(0.5)
+         actions.backward_m(0.7)
          sleep(2)
          status1 = am_i_close(cam, 1)
          
