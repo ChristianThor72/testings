0c8b7d1b40f10e02be3fc939f248d21bec465990
Christian Moller <pst528@alumni.ku.dk>
2022-11-02 11:17:44 +0100

    lavet loop til funktion

--- drivingstategy.py
+++ drivingstategy.py
@@ -45,60 +45,117 @@ status4 = False
 status5 = False
 finished = False
 
+def driving_to_box(current_id, status):
+   while not status:
+      print(f"status {current_id}: ", status)
+      particles = sls.initialize_particles(NUM_PARTICLES)
+
+      est_pose, particles, dist_cam = find_pose(particles, cam, current_id)
+      
+      delta_x = landmarks[current_id][0]*10 - est_pose[0]*10
+      delta_y = landmarks[current_id][1]*10 - est_pose[1]*10
+      dist_mm = np.sqrt((delta_x**2) + (delta_y**2))
+      theta = 0 #vi forventer at vi kigger op kassen
+   #     actions.drive_to_object(dist, 0, 1)
+      safety_dist = 0.2
+      start_time = time.perf_counter()
+      final_dist = max(dist_mm, dist_cam)
+      
+      time_cap = 2.235 * ( float(final_dist*0.001) - safety_dist)
+      print("Dette er distancen: ", final_dist)
+      
+      #Kører imod obejcted og tjekker hele tiden sensor
+      print("DRIVING")
+      arlo.go_diff(70, 70, 1, 1)
+      while True:
+         if (float(time.perf_counter()) - float(start_time)) > time_cap:
+            arlo.stop()
+            break
+         if not (arlo.read_front_ping_sensor() >= safety_dist*1000+5 
+            and arlo.read_left_ping_sensor() >= safety_dist*1000+5 
+            and arlo.read_right_ping_sensor() >= safety_dist**1000+5):
+            arlo.stop()
+            break
+      
+      #Check if it is close to id 2
+      sleep(1)
+      actions.backward_m(0.7)
+      sleep(1)
+      status = am_i_close(cam, current_id)
+      if not status:
+            sleep(1)
+            actions.backward_m(0.25)
+            sleep(1)
+            status = am_i_close(cam, current_id)  
+      
+      if status:
+         est_pose, particles, cam_dists = find_pose(particles, cam, current_id)
+         visited_landmarks.append(current_id)
+
+while not finished: 
+   driving_to_box(1,status1)
+   
+   driving_to_box(2,status2)
+   
+   driving_to_box(3,status3)
+   
+   driving_to_box(4,status4)
+
+   driving_to_box(1,status5)
+
+   print("FINITO")
+"""
 while not finished:
    #self localizing
    est_pose = [0,0,0]
    
    #Hvis den ikke har besøgt landmark 1. 
    while not status1:
-      sleep(1)
       print("status 1: ", status1)
-      #Tager et billede og finder dist til tætteste object med id 1
-      temp_frame = cam.get_next_frame()
-      corners, ids, rejected = cv2.aruco.detectMarkers(temp_frame, dict)
-      print(ids)
-      print("HER: ", corners)
-      if corners:
-         corners, ids = actions.get_corners_ids(1, corners, ids)
-         dist_mm, ang, sign = actions.detector(corners, markerLength, camera_matrix, dist_coeffs)
-         actions.turn_degrees(ang, sign) #Drejer hen imod objected
-         print("DISTDISTDIST",  dist_mm)
-         #Gør klar til at køre imod objected
-         safety_dist = 0.2
-         start_time = time.perf_counter()
-         time_cap = 2.235 * ( float(dist_mm*0.001) - safety_dist)
-         
-         #Kører imod obejcted og tjekker hele tiden sensor
-         print("DRIVING")
-         arlo.go_diff(70, 70, 1, 1)
-         while True:
-            if (float(time.perf_counter()) - float(start_time)) > time_cap:
-               arlo.stop()
-               break
-            if not (arlo.read_front_ping_sensor() >= safety_dist*1000+5 
-               and arlo.read_left_ping_sensor() >= safety_dist*1000+5 
-               and arlo.read_right_ping_sensor() >= safety_dist**1000+5):
-               arlo.stop()
-               break
-         
-         #Check if it is close to id 1
-         sleep(1)
-         actions.backward_m(0.7)
-         sleep(1)
-         status1 = am_i_close(cam, 1)
-         current_id = 1
-         if not status1:
-             sleep(1)
-             actions.backward_m(0.25)
-             sleep(1)
-             status1 = am_i_close(cam, 1) 
-         
-         if status1:
-            est_pose, particles, dist_cam = find_pose(particles, cam, current_id)
-            visited_landmarks.append(1)
-         #If it is not close enough?? Then what? Maybe turn 30 degrees, 
-         # drive 15cm and turn 30 degrees back? As if it is not close enough,
-         # it must be because a object is close to the side sensors. 
+      current_id = 1
+      particles = sls.initialize_particles(NUM_PARTICLES)
+
+      est_pose, particles, dist_cam = find_pose(particles, cam, current_id)
+      
+      delta_x = landmarks[current_id][0]*10 - est_pose[0]*10
+      delta_y = landmarks[current_id][1]*10 - est_pose[1]*10
+      dist_mm = np.sqrt((delta_x**2) + (delta_y**2))
+      theta = 0 #vi forventer at vi kigger op kassen
+   #     actions.drive_to_object(dist, 0, 1)
+      safety_dist = 0.2
+      start_time = time.perf_counter()
+      final_dist = max(dist_mm, dist_cam)
+      
+      time_cap = 2.235 * ( float(final_dist*0.001) - safety_dist)
+      print("Dette er distancen: ", final_dist)
+      
+      #Kører imod obejcted og tjekker hele tiden sensor
+      print("DRIVING")
+      arlo.go_diff(70, 70, 1, 1)
+      while True:
+         if (float(time.perf_counter()) - float(start_time)) > time_cap:
+            arlo.stop()
+            break
+         if not (arlo.read_front_ping_sensor() >= safety_dist*1000+5 
+            and arlo.read_left_ping_sensor() >= safety_dist*1000+5 
+            and arlo.read_right_ping_sensor() >= safety_dist**1000+5):
+            arlo.stop()
+            break
+      
+      #Check if it is close to id 2
+      sleep(1)
+      actions.backward_m(0.7)
+      sleep(1)
+      status1 = am_i_close(cam, current_id)
+      if not status1:
+            sleep(1)
+            actions.backward_m(0.25)
+            sleep(1)
+            status1 = am_i_close(cam, current_id)  
+      
+      if status1:
+         est_pose, particles, cam_dists = find_pose(particles, cam, current_id)
+         visited_landmarks.append(current_id)
 
 
    while not status2:
@@ -138,17 +195,16 @@ while not finished:
       sleep(1)
       actions.backward_m(0.7)
       sleep(1)
-      status2 = am_i_close(cam, 2)
+      status2 = am_i_close(cam, current_id)
       if not status2:
           sleep(1)
           actions.backward_m(0.25)
           sleep(1)
-          status2 = am_i_close(cam, 2)  
+          status2 = am_i_close(cam, current_id)  
       
-      current_id = 2
       if status2:
          est_pose, particles, cam_dists = find_pose(particles, cam, current_id)
-         visited_landmarks.append(2)
+         visited_landmarks.append(current_id)
 
 
    
@@ -301,6 +357,6 @@ while not finished:
       finished = True
       
 print("We did it!!")
-
+"""
 sys.exit()
 
