02c18eedebabc7bc734c684396828fd71b689b50
flemmingfischer <105373590+flemmingfischer@users.noreply.github.com>
2022-11-02 21:35:24 +0100

    Add files via upload

--- flemse_test.py
+++ flemse_test.py
@@ -52,56 +52,72 @@ status5 = False
 finished = False
 
 
-def go_to_box(status, current_id):
+
+def driving_to_box(status, current_id):
     while not status:
-       print(f"status {current_id}: ", status)
-       particles = sls.initialize_particles(NUM_PARTICLES)
+        print(f"status {current_id}: ", status)
+        particles = sls.initialize_particles(NUM_PARTICLES)
+        
+        est_pose, particles, dist_cam = find_pose(particles, cam, current_id)
+        
+        delta_x = landmarks[current_id][0]*10 - est_pose[0]*10
+        delta_y = landmarks[current_id][1]*10 - est_pose[1]*10
+        dist_mm = np.sqrt((delta_x**2) + (delta_y**2))
+        theta = 0 #vi forventer at vi kigger op kassen
+        #actions.drive_to_object(dist, 0, 1)
+        safety_dist = 0.35
+        final_dist = max(dist_mm, dist_cam)
+        
+        time_cap = 2.235 * ( float(final_dist*0.001) - safety_dist)
+        print("Dette er distancen: ", final_dist)
+        
+        #Kører imod obejcted og tjekker hele tiden sensor
+        print("DRIVING")
+        #arlo.go_diff(70, 70, 1, 1)
+        
+        
+        #Søg efter object
+        _ = actions.scan_for_object(cam, dict, current_id)
+        #Når den kan se objektet skal den følgende. 
+        if actions.object_in_site(cam, current_id):
+            actions.drive_to_current_id(cam, time_cap, 350, current_id)
+        #Hvis den ikke kan se objektet, kør i retning af det ud fra pose. 
+        else:
+            actions.going_in_direction_of_box(cam, current_id, est_pose, landmarks, safety_dist, time_cap = 5)
+        
+        #Check if it is close to id
+        sleep(1)
+        actions.backward_m(0.7)
+        sleep(1)
+        status = am_i_close(cam, current_id)
+        if not status:
+            sleep(1)
+            actions.backward_m(0.25)
+            sleep(1)
+            status = am_i_close(cam, current_id)  
+        
+        if status:
+            est_pose, particles, cam_dists = find_pose(particles, cam, current_id)
+            visited_landmarks.append(current_id)
+ 
 
-       est_pose, particles, dist_cam = find_pose(particles, cam, current_id)
-       
-       delta_x = landmarks[current_id][0]*10 - est_pose[0]*10
-       delta_y = landmarks[current_id][1]*10 - est_pose[1]*10
-       dist_mm = np.sqrt((delta_x**2) + (delta_y**2))
-       theta = 0 #vi forventer at vi kigger op kassen
-    #     actions.drive_to_object(dist, 0, 1)
-       safety_dist = 0.4
-       start_time = time.perf_counter()
-       final_dist = max(dist_mm, dist_cam)
-       
-       time_cap = 2.235 * ( float(final_dist*0.001) - safety_dist)
-       print("Dette er distancen: ", final_dist)
-       
-       #Kører imod obejcted og tjekker hele tiden sensor
-       print("DRIVING")
-       #arlo.go_diff(70, 70, 1, 1)
-       while True:
-          arlo.go_diff(40, 40, 1, 1)
-          if (float(time.perf_counter()) - float(start_time)) > time_cap:
-             arlo.stop()
-             break
-          if arlo.read_left_ping_sensor() <= safety_dist*1000:
-             print(arlo.read_left_ping_sensor())
-             actions.turn_degrees(35, 1)
-             sleep(1)
-             actions.forward_m(0.5)
-             sleep(1)
-             actions.turn_degrees(35, -1)
-             sleep(1)
-             
-          if arlo.read_right_ping_sensor() <= safety_dist*1000:
-             print(arlo.read_right_ping_sensor())
-             actions.turn_degrees(35, -1)
-             sleep(1)
-             actions.forward_m(0.5)
-             sleep(1)
-             actions.turn_degrees(35, 1)     
-             sleep(1)
-          
-          if arlo.read_front_ping_sensor() <= safety_dist*1000:
-             arlo.stop()
-             #break
 
-go_to_box(status1, 1)
+            
+driving_to_box(1,status1)
+
+driving_to_box(2,status2)
+
+driving_to_box(3,status3)
+
+driving_to_box(4,status4)
+
+driving_to_box(1,status5)
+
+print("FINITO")
+
+sys.exit()
+
+
 
 
 
