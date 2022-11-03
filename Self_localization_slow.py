f19f483385ea6e7596abdf8e42cce720ca67609b
Flemming Fischer <flemming1997@hotmail.com>
2022-11-03 15:33:35 +0100

    sdds

--- Self_localization_slow.py
+++ Self_localization_slow.py
@@ -59,7 +59,7 @@ CBLACK = (0, 0, 0)
 
 # Landmarks.
 # The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
-landmarkIDs = [1, 2, 3, 4, 5]
+landmarkIDs = [1, 2, 3, 4, 5,6,7,8,9]
 landmarks = {
     1: (0.0, 0.0),  # Coordinates for landmark 1
     2: (0.0, 300.0),  # Coordinates for landmark 2
