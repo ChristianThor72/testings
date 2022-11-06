dca9249090a4b4f2a4a485694e99545d0f174d70
Flemming Fischer <flemming1997@hotmail.com>
2022-11-06 15:17:57 +0100

    sdds

--- actions_final.py
+++ actions_final.py
@@ -313,6 +313,7 @@ def going_in_direction_of_box(cam, current_id, pose, landmarks, safety_dist, tim
     #Turn towards the next object.
     print(pose)
     theta = turn_towards_next_box(pose, current_id, landmarks)
+    print("Theta", theta)
     turn_degrees(theta, -1)
     sleep(3)
     while True:    
