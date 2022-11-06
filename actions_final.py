e4699b810e250775b2c76f72c883bff34e5bf813
Flemming Fischer <flemming1997@hotmail.com>
2022-11-06 19:03:22 +0100

    sdds

--- actions_final.py
+++ actions_final.py
@@ -260,12 +260,12 @@ def drive_to_current_id(cam, time_cap, safety_dist, current_id):
             forward_m(0.5, 40, 40)
             sleep(1)
             turn_degrees(35, -1)
-            sleep(1)
+            sleep(2)
             correct_angle(cam, current_id)
             sleep(0.5)
             time_cap += 5.0
             print("Adding time")
-            sleep(1)
+            
 
         if arlo.read_right_ping_sensor() <= safety_dist:
             print(arlo.read_right_ping_sensor())
@@ -274,12 +274,12 @@ def drive_to_current_id(cam, time_cap, safety_dist, current_id):
             forward_m(0.5, 40, 40)
             sleep(1)
             turn_degrees(35, 1)     
-            sleep(1)
+            sleep(2)
             correct_angle(cam, current_id)
             sleep(0.5)
             time_cap += 5.0
             print("Adding time")
-            sleep(1)
+            
             
         if arlo.read_front_ping_sensor() <= safety_dist+100:
             arlo.stop()
