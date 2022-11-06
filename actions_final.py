7d3a193fa14e08316bdf506601cafbf56e181ef6
Flemming Fischer <flemming1997@hotmail.com>
2022-11-06 19:02:19 +0100

    sdds

--- actions_final.py
+++ actions_final.py
@@ -265,6 +265,7 @@ def drive_to_current_id(cam, time_cap, safety_dist, current_id):
             sleep(0.5)
             time_cap += 5.0
             print("Adding time")
+            sleep(1)
 
         if arlo.read_right_ping_sensor() <= safety_dist:
             print(arlo.read_right_ping_sensor())
@@ -278,6 +279,7 @@ def drive_to_current_id(cam, time_cap, safety_dist, current_id):
             sleep(0.5)
             time_cap += 5.0
             print("Adding time")
+            sleep(1)
             
         if arlo.read_front_ping_sensor() <= safety_dist+100:
             arlo.stop()
